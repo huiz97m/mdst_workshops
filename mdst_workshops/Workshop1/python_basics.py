0ce6f5c9217fb804987eee18907a42630bf11c11
zhihu <zhihu@umich.edu>
2020-02-10 10:13:26 -0500

    Finish implementation of each function.

--- mdst_workshops/Workshop1/python_basics.py
+++ mdst_workshops/Workshop1/python_basics.py
@@ -1,7 +1,8 @@
 """
 MDST Workshop 1 - Python Basics Starter Code
 """
-
+import random
+import base64
 # Add any imports you need here:
 
 
@@ -10,6 +11,14 @@ def part1(num):
     Ask the user for a number. Depending on whether the number is even or odd,
     print out an appropriate (i.e. "even" or "odd") message to the user.
     """
+    in_num = raw_input("Please input an interger ")
+    num = int(in_num)
+    if num % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd!")
+
+
 
 
 def part2():
@@ -23,6 +32,18 @@ def part2():
     [ try checking the random module in python on google. Concepts: Infinite
     loops, if, else, loops and user/input].
     """
+    random_num = random.randrange(1,10)
+    guess_input = input("Choose a random number from 1 to 9 ")
+    while guess_input != "exit":
+        if (guess_input == random_num):
+            print("Exactly right!")
+            break
+        elif(guess_input > random_num):
+            print("Too high!")
+        else:
+            print("Too low!")
+        guess_input = input("Continue guessing...")
+
 
 
 def part3(string):
@@ -31,7 +52,18 @@ def part3(string):
     or not. (A palindrome is a string that reads the same forwards and
     backwards.)
     """
-
+    if string == "":
+        in_word = raw_input("Please enter a word:")
+        string = in_word
+    count = 0
+    for i in range(int(len(string))/2):
+        if string[i] != string[len(string) - i - 1]:
+            print("Not a palindrome")
+            break
+        count += 1
+    if count == int(len(string))/2:
+        print("This is a palindrome.")
+           
 
 def part4a(filename, username, password):
     """
@@ -39,21 +71,42 @@ def part4a(filename, username, password):
     Store your encrypted username on the first line and your encrypted password
     on the second line.
     """
+    encryp_username = base64.standard_b64encode(username)
+    encryp_password = base64.standard_b64encode(password)
+    file = open(filename, "a")
+    file.write(str(encryp_username))
+    file.write("\n")
+    file.write(str(encryp_password))
+    file.write("\n")
+    file.close()
 
-def part4b(filename, password=None):
+
+
+def part4b(filename, password = None):
     """
     Create a function to read the file with your login information.
     Print out the decrypted username and password.
     If a password is specified, update the file with the new password.
     """
+    file = open(filename)
+    file_in_line = file.readlines()
+    username = base64.standard_b64decode(file_in_line[0])
+    orig_password = base64.standard_b64decode(file_in_line[1])
+    print(username)
+    print(orig_password)
+    if(password != None):
+        file_in_line[1] = base64.standard_b64encode(password)
+
+
+
 
 
 if __name__ == "__main__":
-    part1(3)  # odd!
-    part1(4)  # even!
-    part2()
-    part3("ratrace")  # False
-    part3("racecar")  # True
+    #part1(3)  # odd!
+    #part1(4)  # even!
+    #part2()
+    #part3("ratrace")  # False
+    #part3("racecar")  # True
     part4a("secret.txt", "naitian", "p4ssw0rd")
     part4b("secret.txt")
     part4b("secret.txt", password="p4ssw0rd!")
